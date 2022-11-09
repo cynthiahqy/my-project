ef26aed2b1dae3b942ea3a46a2675f615dc744ab
cynthiahqy <29718979+cynthiahqy@users.noreply.github.com>
2022-11-09 22:40:31 +1100

    minor refactor, change ggbump node fill & fontface
    - refactor edges into separate script
    - add case_when for x-from-x to target node fill
        (1-from-1, 1-from-many)
    - map x-to-x to source node fontface (1-to-1, 1-to-many)
    - map min weight to target node fill (split/agg only)

---
+++ edges.R
@@ -0,0 +1,6 @@
+edges <- tibble::tribble(~from, ~to, ~weighted,
+                 "BLX", "BEL", 0.5,
+                 "BLX", "LUX", 0.5,
+                 "E.GER", "DEU", 1,
+                 "W.GER", "DEU", 1,
+                 "AUS", "AUS", 1)
